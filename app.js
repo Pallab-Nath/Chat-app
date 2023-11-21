53cff402776262608e52d8a91999a7448b1328ed
Pallab Nath <pallabnath98@gmail.com>
2023-11-21 19:34:35 +0530

    Basic setup

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const server = require('http').createServer(app)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+// Socket 
+const io = require('socket.io')(server)
+
+io.on('connection', (socket) => {
+    console.log('Connected...')
+    socket.on('message', (msg) => {
+        socket.broadcast.emit('message', msg)
+    })
+
+})
